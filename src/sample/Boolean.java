2b0b020732ea0267017452c5148e9f7ce2e5785c
miyoshiryou <miyoshiryou@192.168.1.24>
2022-03-23 23:50:11 +0900

    First commit

---
+++ src/sample/Boolean.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean a = true;
+		boolean b = false;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
